00fb78dd312bb571470981383151dc7ccef3d53e
jason goodney <jason.goodney@gmail.com>
2021-10-02 12:57:05 -0400

    Initial Commit

---
+++ GitHubFollowersSearch/GitHubFollowersSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubFollowersSearchApp.swift
+//  GitHubFollowersSearch
+//
+//  Created by Jason Goodney on 10/2/21.
+//
+
+import SwiftUI
+
+@main
+struct GitHubFollowersSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
